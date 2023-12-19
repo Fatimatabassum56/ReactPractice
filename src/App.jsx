e281a11e38c72be3fa28de13085fb303a77f2de5
Fatima Tabassum <fatimatabassum743@gmail.com>
2023-12-19 22:00:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <>
+    <div className='mainContainer flex w-[600px] h-[800px] m-11 '>
+      <div className='LeftContainer m-11 mt-11 gap-11 '>
+        <input type='text' placeholder='Email' className="h-[40px] w-[250px]"></input><br></br><br></br>
+
+        <input type='Password' placeholder='Password'className="h-[40px] w-[250px]"></input><br></br><br></br>
+        <button type='Submit' class="bg-gray-400 w-[250px]">Submit</button><br></br><br></br>
+        <p>To know More? <span>Click Here</span></p><br></br>
+    
+      
+        </div>
+        </div>
+
+
+    </>
+
+  )
+}
